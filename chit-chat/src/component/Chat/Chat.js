a45204739cdc068fc29656bb033665e5d609b94d
PrityRastogi16 <surajyoti9839@gmail.com>
2024-02-28 21:02:32 +0530

    messageBox

--- chit-chat/src/component/Chat/Chat.js
+++ chit-chat/src/component/Chat/Chat.js
@@ -1,31 +1,51 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import {user} from "../Join/Join";
 import socketIo from "socket.io-client";
 import "./Chat.css"
+import ReactScrollToBottom from "react-scroll-to-bottom"
+import Message from "../Message/Message"
 const ENDPOINT = "http://localhost:2002";
 let socket;
 const Chat = () => {
+  const [id,setid] = useState("")
+  const [messages,setMessages] = useState([])
   const send=()=>{
-    socket.emit('message',{})
+    const message = document.getElementById("chatInput").value;
+    socket.emit('message',{message,id})
+    document.getElementById("chatInput").value ="";
   }
   useEffect(()=>{
      socket = socketIo(ENDPOINT, {transports:['websocket']})
     socket.on('connect',() => {
       alert("connected");
+      setid(socket.id);
     })
     socket.emit('joined',{user})
     socket.on('welcome', (data)=>{
+      setMessages([...messages,data]);
       console.log(data.user, data.message)
     })
     socket.on('userJoined',(data)=>{
+      setMessages([...messages,data]);
       console.log(data.user, data.message);
     }) 
     socket.on('leave',(data)=>{
+      setMessages([...messages,data]);
       console.log(data.user, data.message);
     })
     return()=>{
-    socket.emit('disconnect');
+    // socket.emit('disconnect');
+    socket.disconnect();
     socket.off();
+    }
+  },[])
+  useEffect(()=>{
+    socket.on('sendMessage',(data)=>{
+      setMessages([...messages,data]);
+     console.log(data.user, data.message, data.id)
+    })
+    return ()=>{
+     
     }
   },[])
   return (
@@ -33,10 +53,12 @@ const Chat = () => {
     <div className='chatPage'>
       <div className='chatContainer'>
         <div className='header'></div>
-        <div className='chatBox'></div>
+        <ReactScrollToBottom className='chatBox'>
+         {messages.map((item,i)=> <Message message={item}/> )}
+        </ReactScrollToBottom>
         <div className='inputBox'>
           <input type="text" id="chatInput" />
-          <button className='sendBtn'>Send</button>
+          <button onClick={send} className='sendBtn'>Send</button>
         </div>
       </div>
     
--- server/index.js
+++ server/index.js
@@ -25,7 +25,10 @@ io.on("connection",(socket)=>{
     socket.broadcast.emit('userJoined',{user:"Admin", message:`${users[socket.id]} has joined`})
     socket.emit('welcome', {user:"Admin", message:`Welcome to the chat, ${users[socket.id]}`})
     })
-    
+    socket.on('message',({message, id})=>{
+         io.emit('sendMessage',{user:users[id],message, id})
+    })
+
     socket.on('disconnect',()=>{
         socket.broadcast.emit('leave',{user:"Admin", message:`${users[socket.id]} has left the chat!`})
         console.log(`User Left!`)
